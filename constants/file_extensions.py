2bd1cfffb40863399a0779e7c849b3b37022d27e
blavikensbutcher <luckertheory@gmail.com>
2024-05-04 13:32:45 +0300

    add sorting with os.walk, del doesnt work

---
+++ constants/file_extensions.py
@@ -0,0 +1,25 @@
+image_extensions = [
+    '.jpg', '.jpeg', '.jpe', '.jif', '.jfif', '.jfi',  # JPEG
+    '.png',                                             # PNG
+    '.gif',                                             # GIF
+    '.bmp', '.dib',                                     # BMP
+    '.tiff', '.tif',                                    # TIFF
+    '.svg', '.svgz',                                    # SVG
+    '.webp',                                            # WebP
+    '.ico', '.cur',                                     # ICO
+    '.tga',                                             # TGA
+    '.dds',                                             # DDS
+    '.hdr', '.exr',                                     # HDR
+    '.pbm', '.pgm', '.ppm',                             # PBM, PGM, PPM
+    '.pnm', '.sr', '.ras', '.jp2', '.j2k', '.jpf',      # PNM, SR, RAS, JP2, J2K, JPF
+    '.jpx', '.jpm', '.mj2',                             # JPX, JPM, MJ2
+    '.heic', '.heif'                                    # HEIC, HEIF
+]
+
+documents_extensions = [
+    '.pdf', '.doc', '.excel', '.txt',
+]
+
+music_extensions = ['.mp3', '.mp4', '.wav', '.aac']
+
+archive_extensions = ['.rar', '.zip']
